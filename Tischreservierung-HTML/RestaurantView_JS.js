c8f184e81d5a703b3f1208f7a3f0cf21f58d8fa5
Unknown <felixkronsteiner@gmx.at>
2018-02-20 14:43:48 +0100

    u
    u

--- Tischreservierung-HTML/RestaurantView_JS.js
+++ Tischreservierung-HTML/RestaurantView_JS.js
@@ -17,7 +17,7 @@ window.onclick = function (event) {
     }
 }
 
-function openCity(evt, cityName) {
+function openSite(evt, element) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("menucontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -27,6 +27,6 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(element).style.display = "block";
     evt.currentTarget.className += " active";
 }
