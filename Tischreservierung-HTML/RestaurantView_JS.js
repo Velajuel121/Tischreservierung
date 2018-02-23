e8b79768b38194bbfb32e29222e44e7d3d5cbc98
Unknown <felixkronsteiner@gmx.at>
2018-02-23 17:44:40 +0100

    Added navigation with Dropdown menu

--- Tischreservierung-HTML/RestaurantView_JS.js
+++ Tischreservierung-HTML/RestaurantView_JS.js
@@ -17,16 +17,13 @@ window.onclick = function (event) {
     }
 }
 
-function openSite(evt, element) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("menucontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+function openContent(contentName, element) {
+    var i, content, tablinks;
+    content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(element).style.display = "block";
-    evt.currentTarget.className += " active";
+    document.getElementById(contentName).style.display = "block";
+
 }
+document.getElementById("default").click();
