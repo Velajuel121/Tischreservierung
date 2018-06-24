2baf74ffc9ecfb7a13525a59697be0e7624def56
Unknown <Alentick@gmail.com>
2018-06-24 20:30:51 +0200

    structured Usage

---
+++ Tischreservierung-HTML/PhpDocker/www/loggedin/readReservations.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- SITE TITTLE -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>TiRe - Admin Mainpage</title>
+    <!-- PLUGINS CSS STYLE -->
+	<link href="plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet">
+	<!-- Bootstrap -->
+	<link href="plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Font Awesome -->
+	<link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Owl Carousel -->
+	<link href="plugins/slick-carousel/slick/slick.css" rel="stylesheet">
+	<link href="plugins/slick-carousel/slick/slick-theme.css" rel="stylesheet">
+	<!-- Fancy Box -->
+	<link href="plugins/fancybox/jquery.fancybox.pack.css" rel="stylesheet">
+	<link href="plugins/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+	<link href="plugins/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css" rel="stylesheet">
+	<!-- CUSTOM CSS -->
+	<link href="css/style.css" rel="stylesheet">
+
+	<!-- FAVICON -->
+	<link href="img/favicon.png" rel="shortcut icon">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="body-wrapper">
+    
+	
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<nav class="navbar navbar-expand-lg  navigation">
+						<a class="navbar-brand" href="index.html">
+							<img src="images/logo.png" alt="">
+						</a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+						 aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav ml-auto main-nav ">
+								<li class="nav-item active">
+									<a class="nav-link" href="index.html">Home</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="reservations.html">Reservierungen</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="aboutus.html">Team</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="rating.html">Bewertung</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="logout.html">Logout</a>
+								</li>
+							</ul>
+							<ul class="navbar-nav ml-auto mt-10">
+								<li class="nav-item">
+									<a class="nav-link login-button" href="login.html">Login</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link add-button" href="registration.html">
+										<i class="fa fa-plus-circle"></i> Registrieren</a>
+								</li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</section>
+    
+    <section>
+        <center><h1>ihre Reservierungen einsehen</h1></center>
+        <form action="readReservations.php" method="post">
+        <div class="container">
+            <div class="row">
+				<div class="col-md-12">
+                    <div class="form-group row">
+                        <label for="input-fromDate" class="col-md-2 col-form-label-lg">Datum: </label>
+                        <div class="col-md-2">
+                            <input class="form-control" type="date" name="input-fromDate">
+                        </div>
+                        <div class="col-md-2">
+                            <center><button type="submit" class="btn btn-primary">Suchen</button></center>
+                        </div>
+				    </div>
+                </div>
+            </div>
+            <?php
+            
+                $servername = "192.168.99.100";
+                $username = "root";
+                $password = "passme";
+                $dbschema = "tischreservierung";
+                $conn = new mysqli($servername, $username, $password,$dbschema);
+
+                // Check connection
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error . "<br>");
+                } 
+            
+                $date = $_POST["input-fromDate"];
+                
+                $sql = "select name,Sitzplaetze,Uhrzeit from reservation where Datum = '".$date."'";
+                $result = $conn->query($sql);
+            
+                if($result->num_rows > 0){
+                    while($row = $result->fetch_assoc()){
+                        echo '<ul class="list-group" style="border-style: solid;">';
+                        echo '  <li class="list-group-item">Name: '.$row["name"].'</li>';
+                        echo '  <li class="list-group-item">Plätze anzahl: '.$row["Sitzplaetze"].'</li>';
+                        echo '  <li class="list-group-item">Uhrzeit: '.$row["Uhrzeit"].'</li>';
+                        echo '</ul>';
+                        echo '<br/>';
+                    }
+                }
+                else{
+                    echo 'Keine Reservierungen an diesem Tag vorhanden';
+                }
+                    
+                $conn->close();  
+                
+            ?>
+        </div>
+        </form>
+    </section>
+
+
+</body>
---
+++ Tischreservierung-HTML/PhpDocker/www/loggedin/redirectPageReservation.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<!-- SITE TITTLE -->
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>TiRe - Reservations</title>
+
+
+	<!-- PLUGINS CSS STYLE -->
+	<link href="plugins/jquery-ui/jquery-ui.min.css" rel="stylesheet">
+	<!-- Bootstrap -->
+	<link href="plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- Font Awesome -->
+	<link href="plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Owl Carousel -->
+	<link href="plugins/slick-carousel/slick/slick.css" rel="stylesheet">
+	<link href="plugins/slick-carousel/slick/slick-theme.css" rel="stylesheet">
+	<!-- Fancy Box -->
+	<link href="plugins/fancybox/jquery.fancybox.pack.css" rel="stylesheet">
+	<link href="plugins/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+	<link href="plugins/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css" rel="stylesheet">
+	<!-- CUSTOM CSS -->
+	<link href="css/style.css" rel="stylesheet">
+
+	<!-- FAVICON -->
+	<link href="img/favicon.png" rel="shortcut icon">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+</head>
+
+<body class="body-wrapper">
+
+    <div class="container">
+        <?php
+
+            $servername = "192.168.99.100";
+            $username = "root";
+            $password = "passme";
+            $dbschema = "tischreservierung";
+            $conn = new mysqli($servername, $username, $password,$dbschema);
+
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+
+            $name = $_POST["input-name"];
+            $persons = $_POST["input-persons"];
+            $date = $_POST["input-date"];
+            $time = $_POST["input-time"];
+
+            $sql = "insert into reservation (name,Sitzplaetze,Uhrzeit,Datum) values ('".$name."', '".$persons."', '".$time."', '".$date."')";
+            if ($conn->query($sql) == TRUE) {
+                 echo '<section>';
+                echo '<div class="container">';
+                
+                echo '<center><h1>Vielen Dank für Ihre Reservierung</h2></center>';
+                echo '<ul class="list-group" style="border-style: solid;">';
+                        echo '  <li class="list-group-item">Name: '.$name.'</li>';
+                        echo '  <li class="list-group-item">Anzahl Personen: '.$persons.'</li>';
+                        echo '  <li class="list-group-item">Datum: '.$date.'</li>';
+                        echo '  <li class="list-group-item">Uhrzeit: '.$time.'</li>';
+                        echo '</ul>';
+                        echo '<br/>';
+                echo '<center><h1>Es wurde Ihnen zusätzlich eine Email mit den Daten geschickt</h1></center>';
+                echo '</div>';
+                echo '</section>';
+
+
+            } else {
+                echo '<section>';
+                echo '<div class="container">';
+                echo '<center><h1>Es tut uns Leid aber etwas ist fehlgeschlagen.</h1></center>';
+                echo '<center><h1>Bitte versuchen Sie es erneut!</h1></center>';
+                echo '</div>';
+                echo '</section>';
+            }
+        
+            echo '<center>';
+            echo '<button onclick="window.location.href=\'http://192.168.99.100/index.html\'" type="submit" class="btn btn-primary">Home</button>';
+            echo '<button style="margin-left: 25px" onclick="window.location.href=\'http://192.168.99.100/reservations.html\'" type="submit" class="btn btn-primary">Zurück zum Reservieren</button>';
+            echo '</div>';
+            echo '</center>';
+        
+            $conn->close();
+    
+        ?>
+    </div>
+    
+    
+    
+	<!-- JAVASCRIPTS -->
+	<script src="plugins/jquery/jquery.min.js"></script>
+	<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+	<script src="plugins/tether/js/tether.min.js"></script>
+	<script src="plugins/raty/jquery.raty-fa.js"></script>
+	<script src="plugins/bootstrap/dist/js/popper.min.js"></script>
+	<script src="plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+	<script src="plugins/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js"></script>
+	<script src="plugins/slick-carousel/slick/slick.min.js"></script>
+	<script src="plugins/jquery-nice-select/js/jquery.nice-select.min.js"></script>
+	<script src="plugins/fancybox/jquery.fancybox.pack.js"></script>
+	<script src="plugins/smoothscroll/SmoothScroll.min.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC72vZw-6tGqFyRhhg5CkF2fqfILn2Tsw"></script>
+	<script src="js/scripts.js"></script>
+
+</body>
+
+</html>
